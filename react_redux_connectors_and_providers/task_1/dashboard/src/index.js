0b9554a1b11753cc146515af049400da4df7bc93
BradenE19 <6115@holbertonstudents.com>
2024-06-05 14:01:08 -0500

    fix test update task1

--- react_redux_connectors_and_providers/task_1/dashboard/src/index.js
+++ react_redux_connectors_and_providers/task_1/dashboard/src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { creatStore, Provider } from "react-redux";
 import App from "./App/App";
+import { uiReducer } from "./reducers/uiReducer";
+
+const store = creatStore(uiReducer)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
